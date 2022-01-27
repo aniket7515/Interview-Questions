17c06fa10dc6ccb6742239c504e3f2d03498963b
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2022-01-27 09:27:04 +0530

    Update question3uniquecharacters.java

--- question3uniquecharacters.java
+++ question3uniquecharacters.java
@@ -6,12 +6,27 @@ public class question3uniquecharacters {
 
         String s="abcdeffghijklm";
         System.out.println(UniqueCharacters(s));
+        System.out.println(UniqueCharactersMethod2(s));
 
     }
     static boolean UniqueCharacters(String s){
         for(int i=0;i<s.length();i++){
             for(int j=i+1;j<s.length();j++){
                 if(s.charAt(i)==s.charAt(j)){
+                    return false;
+                }
+            }
+        }return true;
+    }
+    static boolean UniqueCharactersMethod2(String s){
+        char[] ch=s.toCharArray();
+        for(int i=0;i<s.length();i++){
+            int count=0;
+            for(int j=0;j<s.length();j++){
+                if(ch[i]==ch[j]){
+                    count++;
+                }
+                if(count>1){
                     return false;
                 }
             }
